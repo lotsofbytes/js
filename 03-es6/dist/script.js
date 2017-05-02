9149493e210b0d576b6e1b317c0b453af62551ba
Kenji Hino <kenji@lotsofbytes.com>
2017-05-01 19:53:53 -0700

    updated

--- 03-es6/dist/script.js
+++ 03-es6/dist/script.js
@@ -81,58 +81,57 @@ for (i = 0; i < 36; i++) {
 }
 
 function shuffleArray(array) {
-	for (var i = array.length - 1; i > 0; i--) {
-		var j = Math.floor(Math.random() * (i + 1));
-		var temp = array[i];
+	for (let i = array.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
 	}
 	return array;
 }
 
-function inBox(x, y, target) {
+function inCell(x, y, target) {
 	return x >= target.x && x <= target.x + 90 && y >= target.y && y <= target.y + 90;
 }
 
-function whichBox(boxes, x, y) {
-	var i = 0;
-	for (let box of boxes) {
-		if (inBox(x, y, box)) return i;
+function whichCell(cells, x, y) {
+	let i = 0;
+	for (let cell of cells) {
+		if (inCell(x, y, cell)) return i;
 		i++;
 	}
 
-	return false; // not on any box
+	return false; // not on any cell
 }
 
-function isAdjacentToBlank(boxes, clicked, blank) {
+function isAdjacentToBlank(cells, clicked, blank) {
 	return true;
 }
 
-function swapImageAndBlank(boxes, clicked, blank) {
-	targetImageNum = boxes[clicked].img;
+function swapImageAndBlank(cells, clicked, blank) {
+	targetImageNum = cells[clicked].img;
 
 	console.log('clicked=' + clicked + ' blank=' + blank);
 
-	boxes[clicked].img = null;
-	boxes[blank].img = targetImageNum;
+	cells[clicked].img = null;
+	cells[blank].img = targetImageNum;
 
-	return boxes;
+	return cells;
 }
 
 window.onload = function () {
 
-	var canvas = document.getElementById("canvas"),
+	let canvas = document.getElementById("canvas"),
 	    context = canvas.getContext("2d"),
-	    boxes = [],
+	    cells = [],
 	    blank;
 
 	// randomize numbers for images
-	nums = Array.from(Array(36).keys());
-	random = shuffleArray(nums);
+	random = shuffleArray(Array.from(Array(36).keys()));
 	blank = Math.floor(Math.random() * 34); // random between 0 to 35
 
-	// initialize boxes
-	var b = 0;
+	// initialize cells
+	let b = 0;
 	for ($i = 0; $i <= 5; $i++) {
 		for ($j = 0; $j <= 5; $j++) {
 
@@ -140,7 +139,7 @@ window.onload = function () {
 
 			if (b === blank) imageNum = null;
 
-			boxes[b] = { x: $i * 100, y: $j * 100, img: imageNum };
+			cells[b] = { x: $i * 100, y: $j * 100, img: imageNum };
 			b++;
 		}
 	}
@@ -150,11 +149,11 @@ window.onload = function () {
 
 	function displayGrid() {
 		for (i = 0; i < 36; i++) {
-			if (boxes[i].img === null) {
+			if (cells[i].img === null) {
 				context.fillStyle = "black";
-				context.fillRect(boxes[i].x, boxes[i].y, 90, 90);
+				context.fillRect(cells[i].x, cells[i].y, 90, 90);
 			} else {
-				context.drawImage(images[boxes[i].img], boxes[i].x, boxes[i].y);
+				context.drawImage(images[cells[i].img], cells[i].x, cells[i].y);
 			}
 		}
 	}
@@ -165,16 +164,16 @@ window.onload = function () {
 		x = event.clientX - rect.left;
 		y = event.clientY - rect.top;
 
-		clicked = whichBox(boxes, x, y);
+		clicked = whichCell(cells, x, y);
 
 		if (clicked !== false) {
-			if (isAdjacentToBlank(boxes, clicked, blank)) {
-				boxes = swapImageAndBlank(boxes, clicked, blank);
+			if (isAdjacentToBlank(cells, clicked, blank)) {
+				cells = swapImageAndBlank(cells, clicked, blank);
 				blank = clicked;
 				displayGrid();
 			}
 			//			context.fillStyle = "red";
-			//			context.fillRect(box.x, box.y, 90, 90);
+			//			context.fillRect(cell.x, cell.y, 90, 90);
 		}
 	});
 
@@ -184,7 +183,7 @@ window.onload = function () {
 		x = event.clientX - rect.left;
 		y = event.clientY - rect.top;
 
-		clicked = whichBox(boxes, x, y);
+		clicked = whichCell(cells, x, y);
 
 		if (clicked !== false) {
 			canvas.style.cursor = "pointer";
--- 03-es6/src/script.js
+++ 03-es6/src/script.js
@@ -2,23 +2,23 @@ var images = [];
 
 // preload images
 
-for (i = 0; i < 36; i++) {
+for (let i = 0; i < 36; i++) {
 	j = i + 1; // filename start with 1
 	images[i] = new Image();
 	images[i].src = (j < 10) ? 'images/JoCard_0' + j + '.jpg' : 'images/JoCard_' + j + '.jpg';
 }
 
 function shuffleArray(array) {
-	for (var i = array.length - 1; i > 0; i--) {
-		var j = Math.floor(Math.random() * (i + 1));
-		var temp = array[i];
+	for (let i = array.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
 	}
 	return array;
 }
 
-function inBox(x, y, target) {
+function inCell(x, y, target) {
 	return ((x >= target.x) &&
 		(x <= target.x + 90) &&
 		(y >= target.y) &&
@@ -26,45 +26,44 @@ function inBox(x, y, target) {
 		);
 }
 
-function whichBox(boxes, x, y) {
-	var i = 0;
-	for(let box of boxes) {
-		if (inBox(x, y, box)) return i;
+function whichCell(cells, x, y) {
+	let i = 0;
+	for(let cell of cells) {
+		if (inCell(x, y, cell)) return i;
 		i++;
 	}
 
-	return false; // not on any box
+	return false; // not on any cell
 }
 
-function isAdjacentToBlank(boxes, clicked, blank) {
+function isAdjacentToBlank(cells, clicked, blank) {
 	return true;
 }
 
-function swapImageAndBlank(boxes, clicked, blank) {
-	targetImageNum = boxes[clicked].img;
+function swapImageAndBlank(cells, clicked, blank) {
+	targetImageNum = cells[clicked].img;
 
 	console.log('clicked=' + clicked + ' blank=' + blank);
 
-	boxes[clicked].img = null;
-	boxes[blank].img = targetImageNum;
+	cells[clicked].img = null;
+	cells[blank].img = targetImageNum;
 
-	return boxes;
+	return cells;
 }
 
 window.onload = function () {
 
-	var canvas = document.getElementById("canvas"),
+	let canvas = document.getElementById("canvas"),
 		context = canvas.getContext("2d"),
-		boxes = [],
+		cells = [],
 		blank;
 
 	// randomize numbers for images
-	nums = Array.from(Array(36).keys());
-	random = shuffleArray(nums);
+	random = shuffleArray(Array.from(Array(36).keys()));
 	blank = Math.floor(Math.random() * 34); // random between 0 to 35
 
-	// initialize boxes
-	var b = 0;
+	// initialize cells
+	let b = 0;
 	for($i = 0; $i <= 5; $i++) {
 		for($j = 0; $j <= 5; $j++) {
 
@@ -72,7 +71,7 @@ window.onload = function () {
 
 			if (b === blank) imageNum = null;
 
-			boxes[b] = {x:$i*100, y:$j*100, img: imageNum};
+			cells[b] = {x:$i*100, y:$j*100, img: imageNum};
 			b++;
 		}
 	}
@@ -82,12 +81,12 @@ window.onload = function () {
 
 	function displayGrid() {
 		for (i = 0; i < 36; i++) {
-			if (boxes[i].img === null) {
+			if (cells[i].img === null) {
 				context.fillStyle = "black";
-				context.fillRect(boxes[i].x, boxes[i].y, 90, 90);
+				context.fillRect(cells[i].x, cells[i].y, 90, 90);
 			}
 			else {
-				context.drawImage(images[boxes[i].img], boxes[i].x, boxes[i].y);
+				context.drawImage(images[cells[i].img], cells[i].x, cells[i].y);
 			}
 		}
 	}
@@ -98,16 +97,16 @@ window.onload = function () {
 		x = event.clientX - rect.left;
 		y = event.clientY - rect.top;
 
-		clicked = whichBox(boxes, x, y);
+		clicked = whichCell(cells, x, y);
 
 		if (clicked !== false) {
-			if (isAdjacentToBlank(boxes, clicked, blank)) {
-				boxes = swapImageAndBlank(boxes, clicked, blank);
+			if (isAdjacentToBlank(cells, clicked, blank)) {
+				cells = swapImageAndBlank(cells, clicked, blank);
 				blank = clicked;
 				displayGrid();
 			}
 //			context.fillStyle = "red";
-//			context.fillRect(box.x, box.y, 90, 90);
+//			context.fillRect(cell.x, cell.y, 90, 90);
 		}
 	});
 
@@ -117,7 +116,7 @@ window.onload = function () {
 		x = event.clientX - rect.left;
 		y = event.clientY - rect.top;
 
-		clicked = whichBox(boxes, x, y);
+		clicked = whichCell(cells, x, y);
 
 		if (clicked !== false) {
 			canvas.style.cursor = "pointer";
